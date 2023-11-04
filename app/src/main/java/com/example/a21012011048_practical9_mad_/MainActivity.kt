b3835743a8bb7e32f6719e31cde94aad71a1d708
SANA MANSURI <mansurisana18@gmail.com>
2023-11-04 16:10:38 +0530

    Initial commit

---
+++ app/src/main/java/com/example/a21012011048_practical9_mad_/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.a21012011048_practical9_mad_
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/example/a21012011048_practical9_mad_/splash_Activity.kt
@@ -0,0 +1,52 @@
+package com.example.a21012011048_practical9_mad_
+
+import android.content.Intent
+import android.graphics.drawable.AnimationDrawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+
+class splash_activity : AppCompatActivity() ,Animation.AnimationListener{
+    lateinit var logo_img:ImageView
+    lateinit var logoframbyframanimation:AnimationDrawable
+    lateinit var twinanimation:Animation
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        logo_img=findViewById(R.id.image)
+        logo_img.setBackgroundResource(R.drawable.guni_logo_animation)
+        logoframbyframanimation=logo_img.background as AnimationDrawable
+        twinanimation=AnimationUtils.loadAnimation(this,R.anim.twin_animation)
+        twinanimation.setAnimationListener(this)
+
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if(hasFocus){
+            logoframbyframanimation.start()
+            logo_img.startAnimation(twinanimation)
+        }
+        else{
+            logoframbyframanimation.stop()
+        }
+    }
+
+    override fun onAnimationStart(p0: Animation?) {
+
+    }
+
+    override fun onAnimationEnd(p0: Animation?) {
+
+        intent=Intent(this, MainActivity::class.java).apply {
+            overridePendingTransition(R.anim.scale_in,R.anim.scale_out)
+            startActivity(this) }
+    }
+
+    override fun onAnimationRepeat(p0: Animation?) {
+
+    }
+
+}
